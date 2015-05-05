41f8d23a36a19f7b7074ecdc08acd694a585a425
Brian Scaturro <scaturrob@gmail.com>
2015-05-05 13:32:23 -0400

    add watermark tests

--- __tests__/watermark-test.js
+++ __tests__/watermark-test.js
@@ -1,6 +1,6 @@
 jest.dontMock('../index');
 import {watermark} from '../index';
-import {load} from '../lib/image';
+import {load, fromFiles} from '../lib/image';
 
 describe('watermark', function () {
 
@@ -15,6 +15,28 @@ describe('watermark', function () {
       let first = watermark(promise);
       let second = first.blob((one, two) => one.drawImage(two));
       expect(first).not.toBe(second);
+    });
+  });
+
+  describe('.urls()', function () {
+    it('calls load and returns a new object structure', function () {
+      let first = watermark(promise);
+      let urls = ['url1', 'url2'];
+      let second = first.urls(urls);
+
+      expect(first).not.toBe(second);
+      expect(load).toBeCalledWith(urls, undefined);
+    });
+  });
+
+  describe('.files()', function () {
+    it('calls fromFiles and returns a new object structure', function () {
+      let first = watermark(promise);
+      let fileObjects = [{}, {}];
+      let second = first.files(fileObjects);
+
+      expect(first).not.toBe(second);
+      expect(fromFiles).toBeCalledWith(fileObjects);
     });
   });
 });
