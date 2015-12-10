1ebbb77dfb72429ccae32e86f36b3efa6ec4d323
Brian Scaturro <scaturrob@gmail.com>
2015-12-10 07:37:02 -0500

    remove gulp and use npm scripts

--- gulp/tasks/watch.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var config = require('../config');
-var gulp = require('gulp');
-
-gulp.task('watch', ['build'], function() {
-  return gulp.watch(config.watch.paths, ['build']);
-});
